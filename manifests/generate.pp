0d8590a0f31238d04c141d215b709f6409cd46a1
Paul Edmon <pedmon@DESKTOP-5GBIA4B>
2024-11-04 11:53:37 -0500

    Switching to ensure.

--- manifests/generate.pp
+++ manifests/generate.pp
@@ -1,6 +1,6 @@
 # subuid::generate generates the subuid mapping
 class subuid::generate (
-  Boolean $enable      = false,
+  String  $ensure      = 'absent',
   String  $data_store  = '/odyssey/subuid',
   Integer $subuidstart = 100000000,
   Integer $subuidrange = 66536,
@@ -8,19 +8,19 @@ class subuid::generate (
   Integer $cronmin     = 10,
 ){
 
-  if $enable {
-    file {'/usr/local/sbin/subuid.sh':
-      content => template('subuid/subuid.sh.erb'),
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0755',
-    }
+  file {'/usr/local/sbin/subuid.sh':
+    ensure  => $ensure,
+    content => template('subuid/subuid.sh.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+  }
 
-    cron { 'subuid':
-      command => '/usr/local/sbin/subuid.sh',
-      user    => 'root',
-      hour    => $cronhour,
-      minute  => $cronmin,
-    }
+  cron { 'subuid':
+    ensure  => $ensure,
+    command => '/usr/local/sbin/subuid.sh',
+    user    => 'root',
+    hour    => $cronhour,
+    minute  => $cronmin,
   }
 }
